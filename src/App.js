4c0ce962441dc2a99520cbb56f3f0c26e8545429
bishtjaswant <jaswantbishtuk1995@gmail.com>
2021-05-04 12:09:26 +0530

    first  commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+export default function App(params) {
+    return <>
+    <div className="container">
+        <header>
+            <a href="/">react shopoing store</a>
+        </header>
+        <main>
+            <p>all main content gooes here.........</p>
+        </main>
+        <footer>
+            <p>all right reserved</p>
+        </footer>
+    </div>
+    </>
+};
