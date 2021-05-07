10704b3362a986818c7e18b64d685a7ec892e3a2
bishtjaswant <jaswantbishtuk1995@gmail.com>
2021-05-07 13:08:14 +0530

    add animation and modal

--- src/components/cart/CheckOut.js
+++ src/components/cart/CheckOut.js
@@ -1,9 +1,13 @@
 
 import { useState } from 'react';
-
+import { Fade } from 'react-awesome-reveal';
 
 export default function CheckOut(props) {
-    const {createOrder} = props;
+
+
+    const { createOrder } = props;
+
+
     const [name, setName] = useState("");
     const [email, setEmail] = useState("");
     const [address, setAddress] = useState("");
@@ -11,39 +15,38 @@ export default function CheckOut(props) {
 
     const orderSubmit = (e) => {
         e.preventDefault();
-        let order={name,email,address};
-          createOrder(order);
-
-
-
-
-        //   empty the form data affter subbmiting the form
-          setName("");
-          setEmail("");
-          setAddress("");
+        let order = { name, email, address };
+        createOrder(order);
+                //   empty the form data affter subbmiting the form
+        setName("");
+        setEmail("");
+        setAddress("");
 
     }
 
 
     return <>
-        <div className='checkout-wrapper'>
-            <form method="post" onSubmit={orderSubmit}>
-                <div className="group">
-                    <label htmlFor="">Name</label>
-                    <input required type="text" value={name} onChange={ (e)=>  setName(e.target.value) } name="name" placeholder="name" id="" />
-                </div>
+        <Fade direction='top-left'>
+            <div className='checkout-wrapper'>
+                <form method="post" onSubmit={orderSubmit}>
+                    <div className="group">
+                        <label htmlFor="">Name</label>
+                        <input required type="text" value={name} onChange={(e) => setName(e.target.value)} name="name" placeholder="name" id="" />
+                    </div>
 
-                <div className="group">
-                    <label htmlFor="">Email</label>
-                    <input required type="email" value={email} onChange={ (e)=>  setEmail(e.target.value) } name="email" placeholder="Email address" id="" />
-                </div>
-                <div className="group">
-                    <label htmlFor="">Address</label>
-                    <input required type='text' value={address} onChange={ (e)=> setAddress(e.target.value) } name="address" placeholder="Address" id="" />
-                </div>
+                    <div className="group">
+                        <label htmlFor="">Email</label>
+                        <input required type="email" value={email} onChange={(e) => setEmail(e.target.value)} name="email" placeholder="Email address" id="" />
+                    </div>
+                    <div className="group">
+                        <label htmlFor="">Address</label>
+                        <input required type='text' value={address} onChange={(e) => setAddress(e.target.value)} name="address" placeholder="Address" id="" />
+                    </div>
 
-                <button type='submit'  >checkout</button>
-            </form>
-        </div>
+                    <button type='submit'  >checkout</button>
+                </form>
+            </div>
+
+        </Fade>
     </>
 };
--- src/components/cart/cart.js
+++ src/components/cart/cart.js
@@ -1,63 +1,68 @@
 import React, { Component } from 'react';
 import CartProdutDetail from './CartProdutDetail';
 import CheckOut from './CheckOut';
+import { Bounce, Fade } from 'react-awesome-reveal';
+
+
 class Cart extends Component {
-   
+
     constructor(props) {
         super(props);
-        this.state={
-            checkOut:false
+        this.state = {
+            checkOut: false
         };
     }
-    
-   
+
+
     render() {
-        const {cartItems,createOrder ,clearAll, removeFromCart} = this.props;
-       
-       
+        const { cartItems, createOrder, clearAll, removeFromCart } = this.props;
+
+
         return (
             <>
-            {
-                cartItems.length==0
-                ? 
-                <div> There is no item in cart </div>
-       
-                :
-                  <div>
-                          <div  className='count'>
-                         <div> {cartItems.length} Items </div>
-                         <a onClick={()=>clearAll()} href="#">Clear all</a>
-  
-                          </div>
-                          {
-                              cartItems.map((item, index)=>(
-                             <CartProdutDetail index={index+1} removeFromCart={removeFromCart}  cartItems={item}/>
+                {
+                    cartItems.length == 0
+                        ?
+                        <div> There is no item in cart </div>
 
-                              ))
-                          }
+                        :
+                        <div>
+                            <div className='count'>
+                                <div> {cartItems.length} Items </div>
+                                <a onClick={() => clearAll()} href="#">Clear all</a>
 
-                          <div className="cart-payment">
-                              <div className="total">
-                                  <div className="amt">
-                                      Total:{
-                                          cartItems.reduce((a,c)=> (  a + (c.price*c.count)  ) ,0)
-                                      }
-                                  </div>
-                                  <div className="proceed">
-                                      <button onClick={()=>this.setState({checkOut:!this.state.checkOut})} type="button">proceed</button>
-                                  </div>
-                              </div>
-                          </div>
+                            </div>
+                            {
+                                cartItems.map((item, index) => (
+                                    <CartProdutDetail index={index + 1} removeFromCart={removeFromCart} cartItems={item} />
 
-                          {
-                              this.state.checkOut &&  <CheckOut  createOrder={createOrder}/>
-                          }
-                          
-            
-                  </div> 
+                                ))
+                            }
+
+                            <div className="cart-payment">
+                                <div className="total">
+                                    <div className="amt">
+                                        Total:{
+                                            cartItems.reduce((a, c) => (a + (c.price * c.count)), 0)
+                                        }
+                                    </div>
+                                    <div className="proceed">
+                                        <button onClick={() => this.setState({ checkOut: !this.state.checkOut })} type="button">proceed</button>
+                                    </div>
+                                </div>
+                            </div>
 
 
-            }
+                            <div>
+                                {
+                                  this.state.checkOut && <CheckOut createOrder={createOrder} /> 
+                                }
+                            </div>
+                
+                        </div>
+
+
+                }
 
 
 
